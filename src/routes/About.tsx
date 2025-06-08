67973ae5e4a1a7cb351c9c5dac743ea581bf5679
MSLadanov <MSLadanov@gmail.com>
2025-06-08 22:18:59 +0300

    chore: :art: creating base routes

---
+++ src/routes/About.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react";
+
+export const About = (): ReactElement => {
+  return <main>About</main>;
+};
---
+++ src/routes/Gallery.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react";
+
+export const Gallery = (): ReactElement => {
+  return <main>Gallery</main>;
+};
